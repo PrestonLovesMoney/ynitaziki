da5baa6211b949bbfca20bb5b1f9e48089853145
Official Lsreaam`s github <lsreaam@gmail.com>
2025-07-31 21:41:33 +0300

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+document.querySelector('.explore-btn').addEventListener('click', () => {
+    document.getElementById('shop').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
